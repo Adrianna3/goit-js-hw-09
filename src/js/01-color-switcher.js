17ee28560743e8a3b62c76632d49cb83b3c7f764
Adrianna Staniek <adrstani@wp.pl>
2022-06-11 22:56:30 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+startBtn.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.setAttribute('disabled', 'disabled');
+});
+
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+    startBtn.removeAttribute('disabled', 'disabled');
+});
+
+
 
